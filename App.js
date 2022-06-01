fc065dd74a9ca463535f24b9aa7319a5e41a5db4
RAREBEEF <drrobot409@gmail.com>
2022-06-01 10:49:23 +0900

    Async storage

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,20 +7,24 @@ import {
   TouchableOpacity,
   TextInput,
   ScrollView,
-  Alert,
   Dimensions,
+  ActivityIndicator,
 } from "react-native";
 import { getStatusBarHeight } from "react-native-status-bar-height";
 import { FontAwesome } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const { width: innerWidth } = Dimensions.get("window");
-const StatusBarHeight =
+const { width: INNER_WIDTH } = Dimensions.get("window");
+const STATUS_BAR_HEIGHT =
   Platform.OS === "ios" ? getStatusBarHeight(true) : StatusBar.currentHeight;
+const STORAGE_KEY = "@toDos";
+
 const white = "#e7ecef";
 const black = "#292f36";
 const gray = "#939597";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
   const [isWorking, setIsWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
@@ -28,21 +32,44 @@ export default function App() {
   const onWork = useCallback(() => setIsWorking(true), [setIsWorking]);
   const onPlay = useCallback(() => setIsWorking(false), [setIsWorking]);
 
-  const onChangeText = useCallback(
-    (value) => {
-      setText(value);
-    },
-    [setText]
-  );
+  const onChangeText = (value) => {
+    setText(value);
+  };
 
-  const addToDo = useCallback(() => {
+  const saveToDos = async (toSave) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const loadToDos = async () => {
+    try {
+      const strToDos = await AsyncStorage.getItem(STORAGE_KEY);
+      setToDos(JSON.parse(strToDos));
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    loadToDos();
+  }, []);
+
+  const addToDo = async () => {
     if (text === "") {
       return;
     }
 
-    setToDos({ ...toDos, [Date.now()]: { text, isWorking } });
+    const newToDo = { ...toDos, [Date.now()]: { text, isWorking } };
+
+    setToDos(newToDo);
+    await saveToDos(newToDo);
+
     setText("");
-  }, [setToDos, toDos, setText, text, isWorking]);
+  };
 
   return (
     <View style={styles.container}>
@@ -59,52 +86,58 @@ export default function App() {
           </Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.content}>
-        <TextInput
-          blurOnSubmit={false}
-          value={text}
-          onChangeText={onChangeText}
-          onSubmitEditing={addToDo}
-          style={styles.input}
-          returnKeyType="done"
-          placeholderTextColor={gray}
-          placeholder={isWorking ? "Add To Work" : "Add To Play"}
-        />
-        <ScrollView>
-          {Object.keys(toDos).map((id) =>
-            toDos[id].isWorking === isWorking ? (
-              <View style={styles.toDo}>
-                <ScrollView
-                  horizontal
-                  showsHorizontalScrollIndicator={false}
-                  pagingEnabled
-                  bounces={false}
-                >
-                  <View
-                    style={styles.toDoTextSection}
-                    id={id}
-                    activeOpacity={0.8}
-                    key={id}
+      {loading ? (
+        <View style={styles.loading}>
+          <ActivityIndicator color={black} size="large" />
+        </View>
+      ) : (
+        <View style={styles.content}>
+          <TextInput
+            blurOnSubmit={false}
+            value={text}
+            onChangeText={onChangeText}
+            onSubmitEditing={addToDo}
+            style={styles.input}
+            returnKeyType="done"
+            placeholderTextColor={gray}
+            placeholder={isWorking ? "Add To Work" : "Add To Play"}
+          />
+          <ScrollView>
+            {Object.keys(toDos).map((id) =>
+              toDos[id].isWorking === isWorking ? (
+                <View style={styles.toDo} key={id} id={id}>
+                  <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    pagingEnabled
+                    bounces={false}
                   >
-                    <Text style={styles.toDoText}>{toDos[id].text}</Text>
-                  </View>
-                  <TouchableOpacity
-                    style={styles.toDoMenuSection}
-                    activeOpacity={0.8}
-                  >
-                    <FontAwesome name="trash-o" size={24} color={white} />
-                  </TouchableOpacity>
-                </ScrollView>
-              </View>
-            ) : null
-          )}
-        </ScrollView>
-      </View>
+                    <View style={styles.toDoTextSection} activeOpacity={0.8}>
+                      <Text style={styles.toDoText}>{toDos[id].text}</Text>
+                    </View>
+                    <TouchableOpacity
+                      style={styles.toDoMenuSection}
+                      activeOpacity={0.8}
+                    >
+                      <FontAwesome name="trash-o" size={24} color={white} />
+                    </TouchableOpacity>
+                  </ScrollView>
+                </View>
+              ) : null
+            )}
+          </ScrollView>
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  loading: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
     backgroundColor: white,
@@ -113,7 +146,7 @@ const styles = StyleSheet.create({
   header: {
     justifyContent: "space-between",
     flexDirection: "row",
-    marginTop: StatusBarHeight + 10,
+    marginTop: STATUS_BAR_HEIGHT + 10,
   },
   btnText: {
     color: gray,
@@ -123,18 +156,18 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: black,
     color: white,
-    paddingVertical: 10,
-    paddingHorizontal: 10,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
     borderRadius: 15,
     fontSize: 18,
-    marginTop: 10,
-    marginBottom: 15,
+    marginTop: 20,
+    marginBottom: 20,
   },
   content: {
     flex: 1,
   },
   toDo: {
-    width: innerWidth - 40,
+    width: INNER_WIDTH - 40,
     backgroundColor: black,
     borderRadius: 15,
     marginTop: 5,
@@ -144,7 +177,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingVertical: 15,
     paddingLeft: 20,
-    width: innerWidth,
+    width: INNER_WIDTH,
   },
   toDoMenuSection: {
     paddingVertical: 15,
@@ -152,12 +185,11 @@ const styles = StyleSheet.create({
     backgroundColor: "firebrick",
     borderTopRightRadius: 15,
     borderBottomRightRadius: 15,
+    justifyContent: "center",
   },
   toDoText: {
     color: white,
     fontSize: 16,
-  },
-  toDoMenu: {
-    color: white,
+    paddingRight: 60,
   },
 });
