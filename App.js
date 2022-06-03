81560a8874e93598c8a3519e2999fa2ece00d5c8
RAREBEEF <drrobot409@gmail.com>
2022-06-03 11:34:24 +0900

    Add toDo counter.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -10,6 +10,7 @@ import {
   Dimensions,
   ActivityIndicator,
   Alert,
+  Keyboard,
 } from "react-native";
 import { getStatusBarHeight } from "react-native-status-bar-height";
 import { FontAwesome, FontAwesome5, AntDesign } from "@expo/vector-icons";
@@ -17,12 +18,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import DraggableFlatList, {
   ScaleDecorator,
 } from "react-native-draggable-flatlist";
+import * as SplashScreen from "expo-splash-screen";
 
 const { width: INNER_WIDTH } = Dimensions.get("window");
 const STATUS_BAR_HEIGHT =
   Platform.OS === "ios" ? getStatusBarHeight(true) : StatusBar.currentHeight;
 const STORAGE_KEY = "@toDos";
-
 const white = "#e7ecef";
 const black = "#292f36";
 const gray = "#939597";
@@ -31,17 +32,54 @@ const deleteBtn = "firebrick";
 const doneColor = "#ced4da";
 
 export default function App() {
+  const scrollRef = useRef(null);
+
+  const [keyboard, setKeyboard] = useState([false, 0]);
   const [loading, setLoading] = useState(true);
   const [isWorking, setIsWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState([]);
-  const [scrolled, setScrolled] = useState(false);
+  const [doneCount, setDoneCount] = useState({
+    work: { done: 0, notDone: 0 },
+    play: { done: 0, notDone: 0 },
+  });
 
-  const onWork = useCallback(() => setIsWorking(true), [setIsWorking]);
-  const onPlay = useCallback(() => setIsWorking(false), [setIsWorking]);
+  const onWork = () => setIsWorking(true);
+  const onPlay = () => setIsWorking(false);
 
   const onChangeText = (value) => {
     setText(value);
+  };
+
+  const changeDoneCount = (
+    workDone = 0,
+    workNotDone = 0,
+    playDone = 0,
+    playNotDone = 0
+  ) => {
+    setDoneCount((prev) => {
+      console.log({
+        work: {
+          done: prev.work.done + workDone,
+          notDone: prev.work.notDone + workNotDone,
+        },
+        play: {
+          done: prev.play.done + playDone,
+          notDone: prev.play.notDone + playNotDone,
+        },
+      });
+
+      return {
+        work: {
+          done: prev.work.done + workDone,
+          notDone: prev.work.notDone + workNotDone,
+        },
+        play: {
+          done: prev.play.done + playDone,
+          notDone: prev.play.notDone + playNotDone,
+        },
+      };
+    });
   };
 
   const saveToDos = async (toSave) => {
@@ -61,6 +99,33 @@ export default function App() {
       const strToDos = await AsyncStorage.getItem(STORAGE_KEY);
       const arrToDos = JSON.parse(strToDos);
 
+      setDoneCount({
+        work: { done: 0, notDone: 0 },
+        play: { done: 0, notDone: 0 },
+      });
+
+      let workDone = 0;
+      let workNotDone = 0;
+      let playDone = 0;
+      let playNotDone = 0;
+
+      arrToDos.forEach((toDo) => {
+        if (toDo.isWorking) {
+          if (toDo.isDone) {
+            workDone += 1;
+          } else {
+            workNotDone += 1;
+          }
+        } else {
+          if (toDo.isDone) {
+            playDone += 1;
+          } else {
+            playNotDone += 1;
+          }
+        }
+      });
+
+      changeDoneCount(workDone, workNotDone, playDone, playNotDone);
       setToDos(arrToDos);
       setLoading(false);
     } catch (error) {
@@ -77,33 +142,62 @@ export default function App() {
       return;
     }
 
-    const newToDos = [
-      ...toDos,
-      { key: Date.now(), text, isWorking, isDone: false },
-    ];
+    const newToDos = [...toDos];
+
+    newToDos.splice(doneCount.play.notDone + doneCount.work.notDone, 0, {
+      key: Date.now(),
+      text,
+      isWorking,
+      isDone: false,
+    });
+
+    if (isWorking) {
+      changeDoneCount(0, 1, 0, 0);
+    } else {
+      changeDoneCount(0, 0, 0, 1);
+    }
 
     setToDos(newToDos);
     await saveToDos(newToDos);
 
     setText("");
+
+    scrollRef.current.scrollToEnd();
   };
 
   const deleteToDo = async (key) => {
     const newToDos = [];
 
+    let isDone = false;
+
     toDos.forEach((toDo) => {
       if (toDo.key !== key) {
         newToDos.push(toDo);
+
+        return;
       }
+
+      isDone = toDo.isDone;
     });
 
     setToDos(newToDos);
+
+    if (isWorking) {
+      if (isDone) {
+        changeDoneCount(-1, 0, 0, 0);
+      } else {
+        changeDoneCount(0, -1, 0, 0);
+      }
+    } else {
+      if (isDone) {
+        changeDoneCount(0, 0, -1, 0);
+      } else {
+        changeDoneCount(0, 0, 0, -1);
+      }
+    }
+
     await saveToDos(newToDos);
   };
-
-  useEffect(() => {
-    loadToDos();
-  }, []);
 
   const checkToDo = async (index) => {
     const newToDos = [...toDos];
@@ -112,17 +206,50 @@ export default function App() {
       const updateToDo = { ...toDos[index], isDone: true };
       newToDos.splice(index, 1);
       newToDos.push(updateToDo);
+
+      if (isWorking) {
+        changeDoneCount(1, -1, 0, 0);
+      } else {
+        changeDoneCount(0, 0, 1, -1);
+      }
     } else {
       const updateToDo = { ...toDos[index], isDone: false };
       newToDos.splice(index, 1);
       newToDos.unshift(updateToDo);
+
+      if (isWorking) {
+        changeDoneCount(-1, 1, 0, 0);
+      } else {
+        changeDoneCount(0, 0, -1, 1);
+      }
     }
 
     setToDos(newToDos);
     await saveToDos(newToDos);
   };
 
+  useEffect(() => {
+    const keyboardShow = (e) => {
+      setKeyboard([true, e.endCoordinates.height]);
+    };
+    const keyboardHide = (e) => {
+      setKeyboard([false, e.endCoordinates.height]);
+    };
+
+    loadToDos();
+
+    Keyboard.addListener("keyboardDidShow", keyboardShow);
+    Keyboard.addListener("keyboardDidHide", keyboardHide);
+
+    return () => {
+      Keyboard.removeAllListeners("keyboardDidShow");
+      Keyboard.removeAllListeners("keyboardDidHide");
+    };
+  }, []);
+
   const toDoItem = ({ item, drag, isActive, index }) => {
+    const [open, setOpen] = useState(false);
+
     if (loading) {
       return;
     }
@@ -161,18 +288,27 @@ export default function App() {
             <View
               style={{
                 ...styles.toDoTextSection,
-                width: item.isDone ? INNER_WIDTH - 80 : INNER_WIDTH - 145,
+                width: item.isDone ? INNER_WIDTH - 100 : INNER_WIDTH - 145,
+                marginRight: item.isDone ? 20 : 0,
               }}
             >
-              <Text
-                style={{
-                  ...styles.toDoText,
-                  color: item.isDone ? gray : white,
-                  textDecorationLine: item.isDone ? "line-through" : "none",
+              <TouchableOpacity
+                onPress={() => {
+                  setOpen((prev) => !prev);
                 }}
+                activeOpacity={0.8}
               >
-                {item.text}
-              </Text>
+                <Text
+                  style={{
+                    ...styles.toDoText,
+                    color: item.isDone ? gray : white,
+                    textDecorationLine: item.isDone ? "line-through" : "none",
+                    height: open ? "auto" : 19.5,
+                  }}
+                >
+                  {item.text}
+                </Text>
+              </TouchableOpacity>
             </View>
             {!item.isDone && (
               <TouchableOpacity
@@ -223,6 +359,15 @@ export default function App() {
             </Text>
           </TouchableOpacity>
         </View>
+        <View style={styles.counter}>
+          <Text style={styles.counterText}>
+            Done : {isWorking ? doneCount.work.done : doneCount.play.done}
+          </Text>
+          <Text style={styles.counterText}>
+            Not done :{" "}
+            {isWorking ? doneCount.work.notDone : doneCount.play.notDone}
+          </Text>
+        </View>
         <TextInput
           blurOnSubmit={false}
           value={text}
@@ -239,8 +384,14 @@ export default function App() {
           <ActivityIndicator color={black} size="large" />
         </View>
       ) : (
-        <View style={styles.content}>
+        <View
+          style={{
+            ...styles.content,
+            paddingBottom: keyboard[0] ? keyboard[1] : 10,
+          }}
+        >
           <DraggableFlatList
+            ref={scrollRef}
             containerStyle={styles.list}
             data={toDos}
             onDragEnd={({ data }) => {
@@ -272,7 +423,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingBottom: 10,
   },
-  tab: { flexDirection: "row", justifyContent: "space-between" },
+  tab: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  counter: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  counterText: {
+    fontSize: 18,
+    fontWeight: "500",
+  },
   btnText: {
     color: gray,
     fontSize: 44,
@@ -298,7 +461,8 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     backgroundColor: black,
     borderRadius: 15,
-    marginTop: 10,
+    marginTop: 5,
+    marginBottom: 5,
     overflow: "hidden",
   },
   checkbox: {
@@ -314,6 +478,7 @@ const styles = StyleSheet.create({
   toDoText: {
     color: white,
     fontSize: 16,
+    justifyContent: "center",
   },
   grip: {
     justifyContent: "center",
